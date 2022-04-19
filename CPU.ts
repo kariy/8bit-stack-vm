748062a502bc6beb684c89bd1a13d75bf0d76616
kari <ammr.arf@gmail.com>
2022-04-20 04:03:08 +0800

    add execution hook and debug options

--- CPU.ts
+++ CPU.ts
@@ -1,6 +1,6 @@
 import { Stack } from "./Stack";
 import { Memory } from "./Memory";
-import { INSTRUCTIONS } from "./instructions";
+import { OPCODE } from "./instructions";
 
 interface ICPU {
 	readonly stack: Stack;
@@ -12,6 +12,12 @@ interface ICPU {
 	execute(instruction: Uint8Array): void;
 }
 
+export type TCPUConfig = {
+	enable: boolean;
+	showStack: boolean;
+	showMemory: boolean;
+};
+
 export class CPU implements ICPU {
 	public readonly stack: Stack;
 
@@ -19,72 +25,108 @@ export class CPU implements ICPU {
 
 	public pc: number;
 
-	private FLAGS = {
-		STOP: false,
+	private _debug: TCPUConfig = {
+		enable: false,
+		showStack: true,
+		showMemory: true,
 	};
 
-	constructor(stackSize: number, memorySize: number) {
+	/**
+	 *
+	 * @param stackSize size of the stack
+	 * @param memorySize size of the memory in bytes
+	 */
+	constructor(stackSize: number, memorySize: number, debug?: TCPUConfig) {
 		this.pc = 0;
 		this.stack = new Stack(stackSize);
 		this.memory = new Memory(memorySize);
+
+		if (debug) Object.assign(this._debug, debug);
 	}
 
 	public execute(program: Uint8Array): void {
-		while (!this.FLAGS.STOP) {
-			if (this.pc >= program.length) break;
+		while (this.pc >= 0 && this.pc < program.length) {
+			this._beforeExecuteInstruction();
 
-			console.log(
-				`Executing instruction ${this.pc} : ${program[this.pc]}`
-			);
+			this._executeInstruction(program);
 
-			switch (program[this.pc]) {
-				case INSTRUCTIONS.STOP: {
-					this.FLAGS.STOP = true;
-					break;
-				}
+			this._afterExecuteInstruction();
+		}
+	}
 
-				case INSTRUCTIONS.PUSH: {
-					const _value = new Uint8Array([program[this.pc + 1]]);
-
-					this.stack.push(_value);
-					this.pc++;
-
-					break;
-				}
-
-				case INSTRUCTIONS.POP: {
-					this.stack.pop();
-					break;
-				}
-
-				case INSTRUCTIONS.ADD: {
-					const a = this.stack.pop();
-					const b = this.stack.pop();
-					const _result = a[0] + b[0];
-
-					this.stack.push(new Uint8Array([_result]));
-
-					break;
-				}
-
-				case INSTRUCTIONS.MSTORE: {
-					const _offset = this.stack.pop();
-					const _value = this.stack.pop();
-
-					this.memory.store(_offset[0], _value);
-
-					break;
-				}
-
-				default:
-					throw new Error("Unknown instruction");
+	private _executeInstruction(instructions: Uint8Array): void {
+		switch (instructions[this.pc]) {
+			case OPCODE.STOP: {
+				this.pc = -1;
+				break;
 			}
 
-			this.pc++;
+			case OPCODE.PUSH: {
+				const _value = new Uint8Array([instructions[this.pc + 1]]);
 
-			console.log(this.stack.buffer);
+				this.stack.push(_value);
+				this.pc++;
+
+				break;
+			}
+
+			case OPCODE.POP: {
+				this.stack.pop();
+				break;
+			}
+
+			case OPCODE.ADD: {
+				const a = this.stack.pop();
+				const b = this.stack.pop();
+				const _result = a[0] + b[0];
+
+				this.stack.push(new Uint8Array([_result]));
+
+				break;
+			}
+
+			case OPCODE.MSTORE: {
+				const _offset = this.stack.pop();
+				const _value = this.stack.pop();
+
+				this.memory.store(_offset, _value);
+
+				break;
+			}
+
+			case OPCODE.MLOAD: {
+				const _valueInMemory = this.memory.load(this.stack.pop());
+				this.stack.push(_valueInMemory);
+				break;
+			}
+
+			default:
+				throw new Error("Unknown instruction");
 		}
+	}
 
-		console.log(`Memory : `, this.memory.buffer);
+	private _beforeExecuteInstruction(instructions?: Uint8Array): void {
+		if (this._debug.enable) {
+			if (instructions) {
+				console.log(`PC : [ ${this.pc} ]`);
+				console.log(
+					`Instruction : [ ${OPCODE[instructions[this.pc]]} ]`
+				);
+			}
+		}
+	}
+
+	private _afterExecuteInstruction(instructions?: Uint8Array): void {
+		this.pc++;
+
+		if (this._debug.enable) {
+			if (this._debug.showStack)
+				console.log("Stack :", this.stack.buffer);
+
+			if (this._debug.showMemory)
+				console.log("Memory :", this.memory.buffer);
+
+			console.log("");
+		}
 	}
 }
